4f62292b84d097b15f6bbd47bae5b09e3be5a60d
Melissa C <66797764+Wednesdayw@users.noreply.github.com>
2020-08-24 16:19:57 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+
+
+
+import Nav from './Navbar'
+import SignUp from './SignUp'
+import SignInSide from './SignInSide';
+
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+    
+
+    <Router>
+      <div className="App">
+        <Nav />
+      
+          <Route exact path='/' component={SignInSide} />
+     
+
+      </div>
+    </Router>
   );
+
 }
 
 export default App;
